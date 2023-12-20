a1451ab22a4ea89a5c7ceb2db4e9babead0a75ae
gracecatk <gracie.kiker@gmail.com>
2023-12-19 21:27:49 -0600

    fixed seeds with tutor

--- employee-tracker/seeds.sql
+++ employee-tracker/seeds.sql
@@ -1,26 +1,34 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
-USE company_db;
 
-CREATE TABLE departments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-);
 
-CREATE TABLE roles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  salary DECIMAL(10, 2) NOT NULL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES departments(id)
-);
 
-CREATE TABLE employees (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
+INSERT INTO departments
+(name)
+    VALUES 
+    ( "Executive"),
+    ( "Management"),
+    ( "Administrative"),
+    ( "Security"),
+    ( "Medical");
+
+    INSERT INTO roles 
+( title, salary, department_id)
+    VALUES 
+    ( "Captain", 5000, 1),
+    ( "Cmdr", 2500, 2),
+    ( "Lt", 2250, 2),
+    ( "Lt Cmdr", 2250, 3),
+    ( "Lt Cmdrs", 2350, 3),
+    ( "Counselor", 3000, 4),
+    ( "Doctor", 2150, 2);
+
+
+
+INSERT INTO employees ( first_name, last_name, role_id,  manager_id )
+    VALUES 
+    ( "Jean-Luc", "Picard", 4, 1),
+    ( "William", "Riker", 4, 2),
+    ( "Worf", "worst", 4,1),
+    ( "Data", "dood", 2, NULL),
+    ( "Geordi", "La Forge", 2, 3),
+    ( "Deanna", "Troi", 1, 6),
+    ( "Beverly", "Crusher", 3, NULL);
